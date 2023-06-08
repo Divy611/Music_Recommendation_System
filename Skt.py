f256e399db8e5bb1e3ddc13376cb0d740ebc99b4
Divy611 <divy.parikh@hotmail.com>
2023-06-09 03:31:42 +0530

    GUI Branch created

--- Skt.py
+++ Skt.py
@@ -2,51 +2,50 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+import tkinter as tk
+from tkinter import messagebox
 
-# Load the music dataset
+window = tk.Tk()
+window.title("Music Recommendation System")
+
 data = pd.read_csv(
     'D://VSCode//Python//Music_Recommendation_System//music.csv.zip')
 
-
-print("Music Dataset:")  # Display the dataset
-print(data)
-
-
 # Prepare the input features (X) and the target variable (Y)
 X = data.drop(columns=['genre'])
-
-
-print("\nInput Features:")  # Display the input features
-print(X)
-
 Y = data['genre']
-
-
 X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=0.2, random_state=42)  # Split the dataset into training and testing sets
-model = DecisionTreeClassifier()  # Create and train the decision tree classifier
+    X, Y, test_size=0.2, random_state=42)
+model = DecisionTreeClassifier()
 model.fit(X_train, Y_train)
 
-# Make predictions on the test set
-predictions = model.predict(X_test)
+
+def predict_genre():
+    age = int(age_entry.get())
+    gender = gender_var.get()
+    user_data = pd.DataFrame({'age': [age], 'gender': [gender]})
+    user_prediction = model.predict(user_data)
+    messagebox.showinfo(
+        "Prediction", f"The predicted genre is: {user_prediction[0]}")  # Display the predicted genre
 
 
-results = pd.DataFrame(
-    {'Actual Genre': Y_test, 'Predicted Genre': predictions})  # Display the predicted genres for test data
-print("\nPredicted Genres for Test Data:")
-print(results)
+# Create labels and entries
+age_label = tk.Label(window, text="Age:")
+age_label.pack()
+age_entry = tk.Entry(window)
+age_entry.pack()
 
+gender_label = tk.Label(window, text="Gender:")
+gender_label.pack()
+gender_var = tk.StringVar()
+gender_radio_male = tk.Radiobutton(
+    window, text="Male", variable=gender_var, value="1")
+gender_radio_male.pack()
+gender_radio_female = tk.Radiobutton(
+    window, text="Female", variable=gender_var, value="0")
+gender_radio_female.pack()
 
-user_age = int(input("\nEnter the age: "))  # User input for age and gender
-user_gender = input("Enter the gender (M/F): ")
+predict_button = tk.Button(window, text="Predict", command=predict_genre)
+predict_button.pack()
 
-# Prepare input data for the user
-user_data = pd.DataFrame({'age': [user_age], 'gender': [user_gender]})
-
-# Make prediction for the user input
-user_prediction = model.predict(user_data)
-
-
-# Display the predicted genre for the user input
-print("\nPredicted Genre :")
-print(user_prediction[0])
+window.mainloop()
